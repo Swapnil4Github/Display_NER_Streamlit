8f99d41e5c7b629b66b2362f08007165e4aa48f1
Swapnil Srivastava <62797602+Swapnil4Github@users.noreply.github.com>
2021-04-13 01:03:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+## Packages required for project
+import streamlit as st
+import streamlit.components.v1 as components
+import requests
+import json
+
+st.title("Backend Python Developer Task")
+st.subheader("Named Entity Recognition on Wikipedia")
+raw_text = st.text_area("Your Text","Enter Text Here")
+raw_text = raw_text.replace(" ", "_")
+## Using the Flask api for performing NER
+your_url = 'http://swapnilsrivastava.pythonanywhere.com/'+raw_text
+if st.button("Submit"):
+    result = requests.get(your_url).content
+    html_dict = json.loads(result)
+    web_page = html_dict['html_data']
+    ## Using Streamlit to display the Result
+    st.components.v1.html(web_page,
+       height=3000,
+       width=750)
